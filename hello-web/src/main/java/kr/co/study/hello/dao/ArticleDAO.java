02aa8e488d85efb55fb3afbbaaf2b197d2307feb
seamusic43 <seamusic43@gmail.com>
2022-12-08 15:22:54 +0900

    MyBatis setting

--- hello-web/src/main/java/kr/co/study/hello/dao/ArticleDAO.java
+++ hello-web/src/main/java/kr/co/study/hello/dao/ArticleDAO.java
@@ -1,19 +1,29 @@
 package kr.co.study.hello.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import kr.co.study.hello.vo.Article;
 
 @Repository
 public class ArticleDAO {
+	
+	@Autowired
+	SqlSession sqlSession;
+	
     public void insertArticle(Article article) {
     	System.out.print(article);
+    	sqlSession.insert("mappers.article-mapper.insertArticle",article);
         System.out.println("Insert OK");
     }
 
 	public Article selectArticleById(String articleId) {
-		// TODO Auto-generated method stub
+		return sqlSession.selectOne("mappers.article-mapper.selectArticleById", articleId);
+		/*
 		Article article = new Article(99, "lee", "테스트 타이틀", "테스트 내용은 없다.");
+
 		return article;
+		*/
 	}
 
 }
